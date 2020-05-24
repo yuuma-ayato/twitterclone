349d8a36877b1f981d741cd458fc6e22295e537b
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-05-24 19:14:24 +0900

    投稿画面の作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,16 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
   end
   def new
+    @tweet = Tweet.new
+  end
+  def create
+    Tweet.create(tweet_params)
+    redirect_to new_tweet_path
+  end
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
   end
 end
