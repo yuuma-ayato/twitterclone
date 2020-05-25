cc86af441367ceb0fe81f816097547b075cba043
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-05-25 14:37:21 +0900

    パーシャルの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update]
   def index
     @tweets = Tweet.all
   end
@@ -14,13 +15,10 @@ class TweetsController < ApplicationController
     end
   end
   def show
-    @tweet = Tweet.find(params[:id])
   end
   def edit
-    @tweet = Tweet.find(params[:id])
   end
   def update
-    @tweet = Tweet.find(params[:id])
     if @tweet.update(tweet_params)
       redirect_to tweets_path, notice: "ツイートを編集しました！"
     else
@@ -30,5 +28,8 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
